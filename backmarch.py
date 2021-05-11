c7215bd8a79363c91fca480cf151e0a78ab64219
ckey3 <coenkey3@gmail.com>
2021-05-11 10:17:19 -0500

    little change

--- backmarch.py
+++ backmarch.py
@@ -7,7 +7,3 @@ pixels = neopixel.NeoPixel(board.D18, 20)
 for i in range(20,0):
         pixels[i] = (255,69,0)
         time.sleep(.25)
-
-for x in range(1):
-        pixels[x] = (255,69,0)
-        time.sleep(.25)
