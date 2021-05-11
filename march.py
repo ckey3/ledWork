f8e4f5da82bd0431b19cf33b559900af307ebf00
ckey3 <coenkey3@gmail.com>
2021-05-11 10:22:37 -0500

    little change

--- march.py
+++ march.py
@@ -5,12 +5,12 @@ import time
 pixels = neopixel.NeoPixel(board.D18, 20)
 
 For i in range(0,20):
-	Pixels[i] = (0,0,0)
+	pixels[i] = (0,0,0)
 
 For x in in range(0,20):
-	Pixels[x] = (255,255,255)
+	pixels[x] = (255,255,255)
 	time.sleep(2.5)
 
 For y in range(0,20):
-	Pixels[y] = (0,0,0)
+	pixels[y] = (0,0,0)
 	time.sleep(2.7)
